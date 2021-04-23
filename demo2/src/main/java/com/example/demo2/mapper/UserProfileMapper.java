a222af97405c6fb4437dbff4cd93202c01fcd159
김창대 <cdkim1226@gmail.com>
2021-04-23 22:14:22 +0900

    Api Mapping

---
+++ demo2/src/main/java/com/example/demo2/mapper/UserProfileMapper.java
@@ -0,0 +1,25 @@
+package com.example.demo2.mapper;
+
+import com.example.demo2.model.UserProfile;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface UserProfileMapper {
+
+    @Select("select * from userprofile where id=#{id}")
+    UserProfile getUserProfile(String id);
+
+    @Select("select * from Userprofile")
+    List<UserProfile> getUserProfileList();
+
+    @Insert("insert into Userprofile values(#{id}, #{name}, #{phone}, #{address}")
+    int insertUserProfile(@Param("id") String id, @Param("name") String name, @Param("phone") String phone, @Param("address") String address);
+
+    @Update("update userprofile set name=#{name}, phone=#{phone}, address=#{address} where id=#{id}")
+    int updateUserProfile(@Param("id") String id, @Param("name") String name, @Param("phone") String phone, @Param("address") String address);
+
+    @Delete("delete from userprofile where id=#{id}")
+    int deleteUserProfile(@Param("id") String id);
+}
